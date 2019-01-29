65974ae9267ab510f8f1b396fe2137db26cfd523
jlbepsi <jlb.epsi@gmail.com>
2019-01-29 17:01:42 +0100

    Modification de l'authentification JWT : authentification par clé publique

--- src/main/java/materiel/service/JwtTokenService.java
+++ src/main/java/materiel/service/JwtTokenService.java
@@ -6,14 +6,20 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.spec.X509EncodedKeySpec;
 
 @Service
 public class JwtTokenService {
 
-    @Value("${jwt.token.secret}")
-    private String plainSecret;
+    @Value("${jwt.token.public_key}")
+    private String publicKeyRessource;
 
 
 
@@ -23,8 +29,29 @@ public class JwtTokenService {
 
     private Key getKey() {
         // Création de la clé
-        byte[] encodedBytes = this.plainSecret.getBytes();
-        return Keys.hmacShaKeyFor(encodedBytes);
+        byte[] publicKey = readPublicKey();
+        if (publicKey != null) {
+            try {
+                X509EncodedKeySpec spec = new X509EncodedKeySpec(publicKey);
+
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                return kf.generatePublic(spec);
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return null;
+    }
+
+    private byte[] readPublicKey() {
+        try {
+            ClassPathResource resource = new ClassPathResource(publicKeyRessource);
+            return Files.readAllBytes(Paths.get(resource.getURI()));
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return null;
     }
 
 }
